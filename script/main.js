0945ccc4203e7e04d96e0cdfc0d290efdfd78161
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-06 16:47:51 +0100

    feat: added some comments

--- script/main.js
+++ script/main.js
@@ -1,16 +1,20 @@
 "use strict";
-
+/* Chiedi quanti km deve viaggare l'utente */
 const travelDistance = prompt("How far are you going to get? (insert km)");
 
+/* chiedi l'età del passeggero */
 const passengerAge = prompt("What's your age? (from 1 to 99)");
 
+/* calcolo prezzo del biglietto in base ai km */
 const ticketPrice = travelDistance * 0.21;
 const ticketPriceFixed = ticketPrice.toFixed(2);
 
+/* sconto junior (età inferiore ai 18 anni) */
 const juniorDiscount = (ticketPrice * 20) / 100;
 const juniorDiscountFixed = juniorDiscount.toFixed(2);
 const juniorFinalPrice = ticketPriceFixed - juniorDiscountFixed;
 
+/* sconto senior (età superiore ai 65 anni) */
 const seniorDiscount = (ticketPrice * 40) / 100;
 const seniorDiscountFixed = seniorDiscount.toFixed(2);
 const seniorFinalPrice = ticketPriceFixed - seniorDiscountFixed;
