cfd9ddd01792b7817351e4ab1bd13422eb0bb5f5
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-06 16:25:45 +0100

    feat: assignment

--- script/main.js
+++ script/main.js
@@ -1,1 +1,27 @@
 "use strict";
+
+const travelDistance = prompt("How far are you going to get? (insert km)");
+console.log(`You're authorized to fare ${travelDistance} km.`);
+
+const passengerAge = prompt(
+  "What's your age? (insert integer number from 1 to 99)"
+);
+console.log(`You are ${passengerAge} years old.`);
+
+const ticketPrice = travelDistance * 0.21;
+const juniorDiscount = (ticketPrice * 20) / 100;
+const seniorDiscount = (ticketPrice * 40) / 100;
+
+if (passengerAge < 18) {
+  console.log(
+    `You have right to a discount of ${juniorDiscount} euros! Total price is ${
+      ticketPrice - juniorDiscount
+    } euros!`
+  );
+} else if (passengerAge > 65) {
+  console.log(
+    `You have right to a discount of ${seniorDiscount} euros! Total price is ${
+      ticketPrice - seniorDiscount
+    } euros!`
+  );
+}
