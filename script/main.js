6c43f1856b886a573ba0ac53b553a4500e9a70b5
danielecarpentiero <carpentierodaniele@gmail.com>
2023-11-06 16:43:57 +0100

    fix: fixed calculus

--- script/main.js
+++ script/main.js
@@ -1,32 +1,30 @@
 "use strict";
 
 const travelDistance = prompt("How far are you going to get? (insert km)");
-console.log(`You're authorized to fare ${travelDistance} km.`);
 
-const passengerAge = prompt(
-  "What's your age? (insert integer number from 1 to 99)"
-);
-console.log(`You are ${passengerAge} years old.`);
+const passengerAge = prompt("What's your age? (from 1 to 99)");
 
 const ticketPrice = travelDistance * 0.21;
 const ticketPriceFixed = ticketPrice.toFixed(2);
 
 const juniorDiscount = (ticketPrice * 20) / 100;
 const juniorDiscountFixed = juniorDiscount.toFixed(2);
+const juniorFinalPrice = ticketPriceFixed - juniorDiscountFixed;
 
 const seniorDiscount = (ticketPrice * 40) / 100;
 const seniorDiscountFixed = seniorDiscount.toFixed(2);
+const seniorFinalPrice = ticketPriceFixed - seniorDiscountFixed;
 
 if (passengerAge < 18) {
   console.log(
-    `You have right to a discount of ${juniorDiscountFixed} euros! Total price is ${
-      ticketPriceFixed - juniorDiscountFixed
-    } euros!`
+    `You're authorized to fare ${travelDistance} km. You are ${passengerAge} years old, so you have right to a discount of ${juniorDiscountFixed} euros! Total price is ${juniorFinalPrice} euros!`
   );
 } else if (passengerAge > 65) {
   console.log(
-    `You have right to a discount of ${seniorDiscountFixed} euros! Total price is ${
-      ticketPriceFixed - seniorDiscountFixed
-    } euros!`
+    `You're authorized to fare ${travelDistance} km. You are ${passengerAge} years old, so you have right to a discount of ${seniorDiscountFixed} euros! Total price is ${seniorFinalPrice} euros!`
+  );
+} else {
+  console.log(
+    `You're authorized to fare ${travelDistance} km. Total price is ${ticketPrice}`
   );
 }
